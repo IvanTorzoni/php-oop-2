0610e0d14596c6552c26c6fc5753f62ecf86283c
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-03 17:06:26 +0200

    feat: created basic product

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
 require_once __DIR__ . "/models/products.php";
+
+$product1 = new Products("tonno", "Rio Mare", "Cane");
+var_dump($product1);
--- models/products.php
+++ models/products.php
@@ -1,3 +1,46 @@
 <?php
 
-echo "test per collegamento";
+class Products
+{
+
+    private string $name;
+    private string $producer;
+    private string $target;
+
+    public function __construct(string $name, string $producer, string $target)
+    {
+        $this->name         = $name;
+        $this->producer     = $producer;
+        $this->target       = $target;
+    }
+
+    /**
+     * Get the value of name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the value of producer
+     *
+     * @return string
+     */
+    public function getProducer()
+    {
+        return $this->producer;
+    }
+
+    /**
+     * Get the value of target
+     *
+     * @return string
+     */
+    public function getTarget()
+    {
+        return $this->target;
+    }
+}
